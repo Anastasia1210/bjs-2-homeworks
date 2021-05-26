ea2aff58065f1ffaf5f2c93cd8bfc3b96d1851b0
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-05-26 21:58:12 +0400

    первая версия_не работает d=0

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,27 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2 - (4 * a * c);
+  let x1 = (-b + Math.sqrt(d)) / 2 * a;
+  let x2 = (-b - Math.sqrt(d)) / 2 * a;
+  let x3 = - (b / 2 * a);
+
+
+  if (d > 0) {
+    arr.push(Math.floor(x1 * 100) / 100);
+    arr.push(Math.floor(x2 * 100) / 100);
+  }
+  if (d < 0) {
+    arr;
+  }
+  if (d = 0) {
+    arr.push(Math.floor(x3 * 100) / 100);
+
+  }
+  return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
