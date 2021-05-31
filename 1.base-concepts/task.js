1efe89aa43e842aceb9312a839268f8a858d2ff3
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-05-31 16:22:33 +0400

    дубль 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -12,18 +12,30 @@ function solveEquation(a, b, c) {
     arr.push(Math.floor(x1 * 100) / 100);
     arr.push(Math.floor(x2 * 100) / 100);
   }
-  if (d < 0) {
-    arr;
-  }
-  if (d = 0) {
+  else if (d === 0) {
     arr.push(Math.floor(x3 * 100) / 100);
-
+  }
+  else {
+    arr;
   }
   return arr;
 }
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  let parameters = [percent, contribution, amount];
+  let parametersNames = ['Процентная ставка', 'Начальный взнос', 'Общая стоимость'];
+
+
+  function checkNumbers() {
+    for (let i = 0; i < parameters.length; i++) {
+      if (isNaN(+parameters[i]) === true){
+        alert ('Параметр ' + parametersNames[i] + ' содержит неправильное значение: ' + parameters[i]);
+      }
+    }
+  }
+  checkNumbers();
+
   let totalAmount;
   let s = amount - contribution;
   let a = new Date();
@@ -32,7 +44,10 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let todayYear = a.getYear();
   let dateYear = date.getYear();
   let terms = (dateYear - todayYear) * 12 + (dateMonth - todayMonth);
-  let p = percent / 100;
+  let p = percent / 100 / 12;
+
+
+
   let payment = s * (p + p / (((1 + p)**terms) - 1));
   let total = payment * terms;
   totalAmount = total.toFixed(2);
