fab54da573d19065642dbe9bb39fb232a8050fdd
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-06-02 22:48:33 +0400

    trial 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
 
+if (arr1.length === arr2.length){
+
+  result = arr1.every(function check(value, index){
+    value === arr2[index];
+  })
+}
   // Ваш код
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+compareArrays([1, 8, 9], [1, 8, 9]);
 
-  // Ваш код
+function advancedFilter(arr) {
+  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map(function multiply(item){
+    return item * 10;
+  });
 
   return resultArr; // array
 }
+advancedFilter([-1,6,-9,3]);
